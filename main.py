1735ee0bd86f0f78afdd6e91319e215d378ef269
Bot Telegram <95747163+Bottom-T@users.noreply.github.com>
2022-02-24 05:06:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from config import Config
  
-Telegram = Client(
+bot = Client(
     "Telegram ID Bot",
     bot_token = os.environ["BOT_TOKEN"],
     api_id = int(os.environ["API_ID"]),
@@ -23,13 +23,16 @@ START_BUTTONS = InlineKeyboardMarkup(
         InlineKeyboardButton('⚙ Join Updates Channel ⚙', url='https://telegram.me/us7a5')
         ]]
     )
-
+#2
 msg = message.text
 url = f"http://apis.xditya.me/write?text={msg}"
-Telegram.send_photo(message.chat.id,url,caption=f"<strong>Done\n@us7a5</strong>",parse_mode="html")
+
+bot_token.send_photo(message.chat.id,url,caption=f"<strong>Done\n@us7a5</strong>",parse_mode="html")
 
 
-@Client.on_message(filters.private & filters.command(["start"]))
+
+
+@bot.on_message(filters.private & filters.command(["start"]))
 async def start(bot, update):
     await update.reply_text(
         text=START_TEXT.format(update.from_user.mention),
