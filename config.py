920e1e18466fa0c48584d6d62142fcf0aa0989b0
Bot Telegram <95747163+Bottom-T@users.noreply.github.com>
2022-02-24 04:38:07 +0200

    Update config.py

--- config.py
+++ config.py
@@ -9,5 +9,3 @@ class Config(object):
     # Get these values from my.telegram.org
     API_ID = int(os.environ.get("API_ID"))
     API_HASH = os.environ.get("API_HASH")
-    # Update Channel Username
-    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL")
